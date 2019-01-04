d94a56fc6831c779d8afd5e5178286d384cd7272
Lucas Huguet <lucashuguet31@gmail.com>
2019-01-04 13:45:41 +0100

    projets

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
-
+import { Tabs, Tab } from 'react-mdl';
 class Projects extends Component {
+
+    constructor(probs) {
+        super(probs);
+        this.state = { activeTab: 0 }
+    }
+
     render() {
         return (
-             <div><h1>Projects page</h1></div>
+             <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>React</Tab>
+                    <Tab>React</Tab>
+                    <Tab>React</Tab>
+                </Tabs>
+             </div>
         )
     }
 }
